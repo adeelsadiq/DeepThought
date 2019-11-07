948be8d4588a02b740f88ac74fb67c2a56091226
Adeel McDangerson <adeelsk4@gmail.com>
2019-11-07 15:28:15 +0000

    progress

--- src/DeepThought/TheKeeper.java
+++ src/DeepThought/TheKeeper.java
@@ -1,11 +1,15 @@
 package DeepThought;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 
-public class TheKeeper extends TheTruth{
-	
+public class TheKeeper extends TheTruth implements ActionListener{
+	String message;
 	public TheKeeper() {
 		
 	}
+	//add history file as well, that just appends everything written to log file to itself but only populates the main window if the history menu button is pressed  
+	//-> also add history menu button too :D 
 	
 	void Keep(String Message) {
 		
@@ -14,15 +18,25 @@ public class TheKeeper extends TheTruth{
 			PrintWriter scribe = new PrintWriter((new FileWriter("test.txt", true)));
 
 	      	scribe.println(Message);
+	      	this.message = Message;
 	      	scribe.close();
 	    }
 	    catch (IOException e){
 	      	System.out.println("Error: " + e.getMessage());
 	    }
 		finally {
-		 	System.out.println("Writer out y'all");
+		 	System.out.println("Writer over and out y'all");
 		}
 
+	}
+	String TheMessage() { // overwrite the parent method
+		
+		return this.message;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
--- src/DeepThought/TheTruth.java
+++ src/DeepThought/TheTruth.java
@@ -6,8 +6,9 @@ public class TheTruth {
 		
 	}
 	
-	String TheMessage() {
+	String TheMessage() { // this I will tie to a random button called the truth or something 
 		return "Seek and you shall find"; //your random profound sounding nonsense 
 	}
+	
 	
 }
